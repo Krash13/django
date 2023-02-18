974768e83f7b4fee70ce33e8993c59be410e0894
Krasheninnikov Roman <KrashR26@gmail.com>
2023-02-18 23:25:47 +0300

    Хост 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
+CSRF_TRUSTED_ORIGINS = ['"https://django-production-4443.up.railway.app"']
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["SECRETS"]
 
@@ -27,7 +27,6 @@ SECRET_KEY = os.environ["SECRETS"]
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "https://django-production-4443.up.railway.app",
     "*"
 ]
 
